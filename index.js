d9bb6b715fdca9e3a3554a14d7486bf1b84465f0
Andy Young <and.you.dev@gmail.com>
2023-12-01 09:21:59 +0000

    animations test

--- index.js
+++ index.js
@@ -3,3 +3,37 @@
 let thisYear = new Date();
 
 $("#dynYear").text(thisYear.getFullYear());
+
+const elements = document.querySelectorAll('.anim-on-scroll');
+
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: .4
+}
+
+const myCallback = (entries) => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      if(entry.target.classList.contains('anim-one')) {
+          entry.target.classList.add('active-one');
+      }
+      if(entry.target.classList.contains('anim-two')) {
+          entry.target.classList.add('active-two');
+      }
+    } else {
+      if(entry.target.classList.contains('anim-one')) {
+          entry.target.classList.remove('active-one');
+      }
+      if(entry.target.classList.contains('anim-two')) {
+          entry.target.classList.remove('active-two');
+      }
+    }
+  });
+}
+
+let observer = new IntersectionObserver(myCallback,options);
+
+elements.forEach(element => {
+  observer.observe(element);
+})
