c6dd30ece5b4960e950dbb3c8b76cad3b9e5db0e
Andy Young <and.you.dev@gmail.com>
2023-12-12 03:43:42 +0000

    final draft before sk and bp

--- index.js
+++ index.js
@@ -14,67 +14,178 @@ const options = {
 
 const myCallback = (entries) => {
   entries.forEach(entry => {
-    if(entry.isIntersecting){
-      if(entry.target.classList.contains('anim-one')) {
-          entry.target.classList.add('active-one');
-      }
-      if(entry.target.classList.contains('anim-two')) {
-          entry.target.classList.add('active-two');
-      }
-      if(entry.target.classList.contains('anim-three')) {
-          entry.target.classList.add('active-three');
-      }
-      if(entry.target.classList.contains('deco-img')) {
-          entry.target.classList.add('active-img');
-      }
-      if(entry.target.classList.contains('banner-anim-trigger')) {
+    if(entry.boundingClientRect.top >= 0) {
+      if(entry.isIntersecting){
+        if(entry.target.classList.contains('anim-one')) {
+            entry.target.classList.add('active-one');
+        }
+        if(entry.target.classList.contains('anim-two')) {
+            entry.target.classList.add('active-two');
+        }
+        if(entry.target.classList.contains('anim-three')) {
+            entry.target.classList.add('active-three');
+        }
+        if(entry.target.classList.contains('deco-img')) {
+            entry.target.classList.add('active-img');
+        }
+        if(entry.target.classList.contains('banner-anim-trigger')) {
+            const banner = document.querySelector('.wand-sticky-top-bar');
+            banner.classList.add('top-bar-anim');
+            const bannerLogo = document.querySelector('.logo-img-landing');
+            bannerLogo.classList.add('logo-anim');
+            //console.log(window.pageYOffset);
+        }
+      } else {
+        if(entry.target.classList.contains('hidden-trigger-two')) {
           const banner = document.querySelector('.wand-sticky-top-bar');
-          banner.classList.add('top-bar-anim');
-          const bannerLogo = document.querySelector('.logo-img-landing');
-          bannerLogo.classList.add('logo-anim');
-          //console.log(window.pageYOffset);
-      }
-    } else {
-      if(entry.target.classList.contains('anim-one')) {
-          entry.target.classList.remove('active-one');
-      }
-      if(entry.target.classList.contains('anim-two')) {
-          entry.target.classList.remove('active-two');
-      }
-      if(entry.target.classList.contains('anim-three')) {
-          entry.target.classList.remove('active-three');
-      }
-      if(entry.target.classList.contains('banner-anim-trigger') && ((window.innerWidth > 1000 && window.pageYOffset < 150) || (window.innerWidth < 1000 && window.pageYOffset < 480))) {
-          const banner = document.querySelector('.wand-sticky-top-bar');
-          banner.classList.remove('top-bar-anim');
-          const bannerLogo = document.querySelector('.logo-img-landing');
-          bannerLogo.classList.remove('logo-anim');
+          if(banner.classList.contains('top-bar-anim')) {
+            banner.classList.remove('top-bar-anim');
+            const bannerLogo = document.querySelector('.logo-img-landing');
+            bannerLogo.classList.remove('logo-anim');
+          }
           const allOnes = document.querySelectorAll('.anim-one');
           if(allOnes) {
             allOnes.forEach(one => {
-              one.classList.remove('active-one');
+              if(one.classList.contains('active-one')) {
+                  one.classList.remove('active-one');
+              }
             });
           }
           const allTwos = document.querySelectorAll('.anim-two');
           if(allTwos) {
             allTwos.forEach(two => {
-              two.classList.remove('active-two');
+              if(two.classList.contains('active-two')) {
+                  two.classList.remove('active-two');
+              }
             });
           }
           const allThrees = document.querySelectorAll('.anim-three');
           if(allThrees) {
             allThrees.forEach(three => {
-              three.classList.remove('active-three');
+              if(three.classList.contains('active-three')) {
+                  three.classList.remove('active-three');
+              }
             });
           }
           const allDecoImgs = document.querySelectorAll('.deco-img');
           if(allDecoImgs) {
             allDecoImgs.forEach(img => {
-              img.classList.remove('active-img');
+              if(img.classList.contains('active-img')) {
+                  img.classList.remove('active-img');
+              }
             });
           }
-
+        }
       }
+    //}
+    // if(entry.isIntersecting){
+    //   if(entry.target.classList.contains('anim-one')) {
+    //       entry.target.classList.add('active-one');
+    //   }
+    //   if(entry.target.classList.contains('anim-two')) {
+    //       entry.target.classList.add('active-two');
+    //   }
+    //   if(entry.target.classList.contains('anim-three')) {
+    //       entry.target.classList.add('active-three');
+    //   }
+    //   if(entry.target.classList.contains('deco-img')) {
+    //       entry.target.classList.add('active-img');
+    //   }
+    //   if(entry.target.classList.contains('banner-anim-trigger')) {
+    //       const banner = document.querySelector('.wand-sticky-top-bar');
+    //       banner.classList.add('top-bar-anim');
+    //       const bannerLogo = document.querySelector('.logo-img-landing');
+    //       bannerLogo.classList.add('logo-anim');
+    //       //console.log(window.pageYOffset);
+    //   }
+    //   if(entry.target.classList.contains('hidden-trigger-two')) {
+    //     const banner = document.querySelector('.wand-sticky-top-bar');
+    //     if(banner.classList.contains('top-bar-anim')) {
+    //       banner.classList.remove('top-bar-anim');
+    //       const bannerLogo = document.querySelector('.logo-img-landing');
+    //       bannerLogo.classList.remove('logo-anim');
+    //     }
+    //     const allOnes = document.querySelectorAll('.anim-one');
+    //     if(allOnes) {
+    //       allOnes.forEach(one => {
+    //         if(one.classList.contains('active-one')) {
+    //             one.classList.remove('active-one');
+    //         }
+    //       });
+    //     }
+    //     const allTwos = document.querySelectorAll('.anim-two');
+    //     if(allTwos) {
+    //       allTwos.forEach(two => {
+    //         if(two.classList.contains('active-two')) {
+    //             two.classList.remove('active-two');
+    //         }
+    //       });
+    //     }
+    //     const allThrees = document.querySelectorAll('.anim-three');
+    //     if(allThrees) {
+    //       allThrees.forEach(three => {
+    //         if(three.classList.contains('active-three')) {
+    //             three.classList.remove('active-three');
+    //         }
+    //       });
+    //     }
+    //     const allDecoImgs = document.querySelectorAll('.deco-img');
+    //     if(allDecoImgs) {
+    //       allDecoImgs.forEach(img => {
+    //         if(img.classList.contains('active-img')) {
+    //             img.classList.remove('active-img');
+    //         }
+    //       });
+    //     }
+    //   }
+    // } //else {
+      // if(entry.target.classList.contains('anim-one')) {
+      //     entry.target.classList.remove('active-one');
+      // }
+      // if(entry.target.classList.contains('anim-two')) {
+      //     entry.target.classList.remove('active-two');
+      // }
+      // if(entry.target.classList.contains('anim-three')) {
+      //     entry.target.classList.remove('active-three');
+      // }
+      //if (document.body.scrollTop == 0 || document.documentElement.scrollTop == 0)
+//       if(window.pageYOffset == 0)
+//           {
+//             console.log("top: " + window.pageYOffset);
+//           //}
+// //      if(entry.target.classList.contains('banner-anim-trigger') && ((window.innerWidth > 1000 && window.pageYOffset < 100) || (window.innerWidth < 1000 && window.pageYOffset < 480))) {
+//         //if(entry.target.classList.contains('banner-anim-trigger')) {
+//           const banner = document.querySelector('.wand-sticky-top-bar');
+//           banner.classList.remove('top-bar-anim');
+//           const bannerLogo = document.querySelector('.logo-img-landing');
+//           bannerLogo.classList.remove('logo-anim');
+//           const allOnes = document.querySelectorAll('.anim-one');
+//           if(allOnes) {
+//             allOnes.forEach(one => {
+//               one.classList.remove('active-one');
+//             });
+//           }
+//           const allTwos = document.querySelectorAll('.anim-two');
+//           if(allTwos) {
+//             allTwos.forEach(two => {
+//               two.classList.remove('active-two');
+//             });
+//           }
+//           const allThrees = document.querySelectorAll('.anim-three');
+//           if(allThrees) {
+//             allThrees.forEach(three => {
+//               three.classList.remove('active-three');
+//             });
+//           }
+//           const allDecoImgs = document.querySelectorAll('.deco-img');
+//           if(allDecoImgs) {
+//             allDecoImgs.forEach(img => {
+//               img.classList.remove('active-img');
+//             });
+//           }
+
+      //}
+  //  }
     }
   });
 }
